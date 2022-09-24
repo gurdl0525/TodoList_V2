cb6338ef5eb508d605ee08ae353a0eeba02d399c
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-24 23:29:59 +0900

    🎲 ADD :: Basic Auth

--- src/main/kotlin/com/example/todolistv2/domain/auth/data/General.kt
+++ src/main/kotlin/com/example/todolistv2/domain/auth/data/General.kt
@@ -1,14 +1,24 @@
 package com.example.todolistv2.domain.auth.data
 
 import com.example.todolistv2.domain.auth.type.Role
+import java.time.LocalDateTime
 
-class Admin(
+class General(
     accountId: String,
     password: String,
-    nickname: String
+    nickname: String,
+    role: Role,
+    createdBy: Long,
+    createdAt: LocalDateTime
 ): User(
     accountId = accountId,
     password = password,
     nickname = nickname,
-    role = Role.ADMIN
-) {}
+    role = role,
+    createdBy = createdBy,
+    createdAt = createdAt
+) {
+    fun editStatus(role: Role){
+        this.roleList.add(role)
+    }
+}
