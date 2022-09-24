b5ba1f8f5b107a647f809a0b0569c1bc48c8c142
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-24 23:30:34 +0900

    🎲 ADD :: Base Author entity

--- src/main/kotlin/com/example/todolistv2/global/base/entity/BaseAuthorEntity.kt
+++ src/main/kotlin/com/example/todolistv2/global/base/entity/BaseAuthorEntity.kt
@@ -1,4 +1,27 @@
 package com.example.todolistv2.global.base.entity
 
-class BaseAuthorEntity {
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+import javax.persistence.EntityListeners
+import javax.persistence.MappedSuperclass
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseAuthorEntity(
+    createdBy: Long,
+    createdAt: LocalDateTime
+):BaseTimeEntity(
+    createdAt = createdAt
+) {
+
+    @CreatedBy
+    open var createdBy: Long = createdBy
+        protected set
+
+    @LastModifiedBy
+    open var updatedBy: Long? = null
+        protected set
+
 }
