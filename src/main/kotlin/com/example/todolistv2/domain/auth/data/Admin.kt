d2de8a6c0f78245894968241cfdb01d28afefaa7
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-24 23:30:58 +0900

    🎲 ADD :: Admin account class

--- src/main/kotlin/com/example/todolistv2/domain/auth/data/Admin.kt
+++ src/main/kotlin/com/example/todolistv2/domain/auth/data/Admin.kt
@@ -1,4 +1,19 @@
 package com.example.todolistv2.domain.auth.data
 
-class Admin {
-}
+import com.example.todolistv2.domain.auth.type.Role
+import java.time.LocalDateTime
+
+class Admin(
+    accountId: String,
+    password: String,
+    nickname: String,
+    createdBy: Long,
+    createdAt: LocalDateTime
+): User(
+    accountId = accountId,
+    password = password,
+    nickname = nickname,
+    role = Role.ADMIN,
+    createdBy = createdBy,
+    createdAt = createdAt
+) {}
