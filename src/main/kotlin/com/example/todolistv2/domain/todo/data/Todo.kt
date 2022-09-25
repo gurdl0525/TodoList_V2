adfc91a5b1cff4f45d9fcc742c2f6709c508bd2a
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-25 23:35:16 +0900

    🎲 ADD :: Todo Entity Generate

---
+++ src/main/kotlin/com/example/todolistv2/domain/todo/data/Todo.kt
@@ -0,0 +1,43 @@
+package com.example.todolistv2.domain.todo.data
+
+import com.example.todolistv2.global.base.entity.BaseAuthorEntity
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.DiscriminatorValue
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.OneToMany
+import javax.validation.constraints.Size
+
+
+@Entity
+class Todo(
+    title: String,
+    content: String,
+    categoryList: MutableList<Category>,
+    createdBy: Long,
+    createdAt: LocalDateTime
+): BaseAuthorEntity(
+    createdBy = createdBy,
+    createdAt = createdAt
+) {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @Column(name = "title", nullable = false)
+    @Size(min = 1, max = 10)
+    var title: String = title
+        protected set
+
+    @Column(name = "content", nullable = false)
+    @Size(min = 4, max = 15)
+    var content: String = content
+        protected set
+
+    @OneToMany(mappedBy = "todo")
+    var categoryList: MutableList<Category>? = categoryList
+        protected set
+}
