6ff11d59f6928efe27383d2a3d96295121019ffc
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-25 23:37:39 +0900

    🎲 ADD :: Annotation add

--- src/main/kotlin/com/example/todolistv2/domain/auth/data/General.kt
+++ src/main/kotlin/com/example/todolistv2/domain/auth/data/General.kt
@@ -2,23 +2,22 @@ package com.example.todolistv2.domain.auth.data
 
 import com.example.todolistv2.domain.auth.type.Role
 import java.time.LocalDateTime
+import javax.persistence.DiscriminatorValue
+import javax.persistence.Entity
 
+@Entity
+@DiscriminatorValue("general")
 class General(
     accountId: String,
     password: String,
     nickname: String,
     role: Role,
-    createdBy: Long,
     createdAt: LocalDateTime
 ): User(
     accountId = accountId,
     password = password,
     nickname = nickname,
     role = role,
-    createdBy = createdBy,
     createdAt = createdAt
 ) {
-    fun editStatus(role: Role){
-        this.roleList.add(role)
-    }
 }
