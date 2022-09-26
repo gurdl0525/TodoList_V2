9debe977cf99ebe1673876ab8cb072511a7f53d6
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-26 23:42:57 +0900

    ♠ UPDATE :: user foreign key

--- src/main/kotlin/com/example/todolistv2/domain/todo/data/Todo.kt
+++ src/main/kotlin/com/example/todolistv2/domain/todo/data/Todo.kt
@@ -1,13 +1,15 @@
 package com.example.todolistv2.domain.todo.data
 
+import com.example.todolistv2.domain.auth.data.entity.User
 import com.example.todolistv2.global.base.entity.BaseAuthorEntity
 import java.time.LocalDateTime
 import javax.persistence.Column
-import javax.persistence.DiscriminatorValue
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 import javax.persistence.OneToMany
 import javax.validation.constraints.Size
 
@@ -18,7 +20,8 @@ class Todo(
     content: String,
     categoryList: MutableList<Category>,
     createdBy: Long,
-    createdAt: LocalDateTime
+    createdAt: LocalDateTime,
+    user: User
 ): BaseAuthorEntity(
     createdBy = createdBy,
     createdAt = createdAt
@@ -39,5 +42,10 @@ class Todo(
 
     @OneToMany(mappedBy = "todo")
     var categoryList: MutableList<Category>? = categoryList
+        protected set
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    var user: User = user
         protected set
 }
