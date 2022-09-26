0b50f6795a15ed813e781cf3a71edabc3c8cfb01
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-26 23:40:28 +0900

    🎲 ADD :: token에서 user추출

---
+++ src/main/kotlin/com/example/todolistv2/global/util/UserFacade.kt
@@ -0,0 +1,22 @@
+package com.example.todolistv2.global.util
+
+import com.example.todolistv2.domain.auth.data.entity.User
+import com.example.todolistv2.domain.auth.repository.UserRepository
+import com.example.todolistv2.global.error.exception.common.UnAuthorizedException
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserFacade(
+    private val userRepository: UserRepository<User>
+) {
+    fun getCurrentUser(): User {
+        val subject = SecurityContextHolder.getContext()
+            .authentication
+            .credentials
+            .toString().toLong()
+        return userRepository.findByIdOrNull(subject)
+            ?:(throw UnAuthorizedException(subject.toString()))
+    }
+}
