e12087bfc4ef52ec4aca7405d6d9dc990f6fdc14
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-27 20:30:16 +0900

    ♠ UPDATE :: login, checkDuplicate구현

--- src/main/kotlin/com/example/todolistv2/domain/auth/service/GeneralService.kt
+++ src/main/kotlin/com/example/todolistv2/domain/auth/service/GeneralService.kt
@@ -1,12 +1,19 @@
 package com.example.todolistv2.domain.auth.service
 
+import com.example.todolistv2.domain.auth.data.dto.request.CheckNicknameRequest
 import com.example.todolistv2.domain.auth.data.dto.request.CreateUserRequest
 import com.example.todolistv2.domain.auth.data.dto.request.EditUserNickNameRequest
+import com.example.todolistv2.domain.auth.data.dto.request.LoginUserRequest
+import com.example.todolistv2.domain.auth.data.dto.response.TokenResponse
 
 interface GeneralService {
 
     fun join(request: CreateUserRequest)
 
     fun editNickname(request: EditUserNickNameRequest)
+
+    fun checkDuplicate(request: CheckNicknameRequest)
+
+    fun login(request: LoginUserRequest): TokenResponse
 
 }
--- src/main/kotlin/com/example/todolistv2/domain/auth/service/GeneralServiceImpl.kt
+++ src/main/kotlin/com/example/todolistv2/domain/auth/service/GeneralServiceImpl.kt
@@ -1,10 +1,14 @@
 package com.example.todolistv2.domain.auth.service
 
+import com.example.todolistv2.domain.auth.data.dto.request.CheckNicknameRequest
 import com.example.todolistv2.domain.auth.data.dto.request.CreateUserRequest
 import com.example.todolistv2.domain.auth.data.dto.request.EditUserNickNameRequest
+import com.example.todolistv2.domain.auth.data.dto.request.LoginUserRequest
+import com.example.todolistv2.domain.auth.data.dto.response.TokenResponse
 import com.example.todolistv2.domain.auth.data.entity.General
 import com.example.todolistv2.domain.auth.repository.UserRepository
 import com.example.todolistv2.domain.auth.data.entity.type.Role
+import com.example.todolistv2.domain.auth.exception.DuplicateNicknameException
 import com.example.todolistv2.global.util.UserFacade
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -19,12 +23,27 @@ class GeneralServiceImpl(
     private val userFacade: UserFacade
 ): GeneralService {
 
+    override fun checkDuplicate(request: CheckNicknameRequest) {
+        if(userRepository.existsByNickname(request.nickname)){
+            throw DuplicateNicknameException(request.nickname)
+        }
+    }
+
+    override fun login(request: LoginUserRequest): TokenResponse {
+        TODO("Not yet implemented")
+    }
+
     override fun join(request: CreateUserRequest) {
+        val nickname = if (userRepository.existsByNickname(request.nickname)){
+            throw DuplicateNicknameException(request.nickname)
+        } else{
+            request.nickname
+        }
         userRepository.save(
             General(
                 accountId = request.accountId,
                 password = passwordEncoder.encode(request.password),
-                nickname = request.nickname,
+                nickname = nickname,
                 role = Role.NORMAL,
                 createdAt = LocalDateTime.now()
         ))
