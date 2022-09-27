03bf1410cb35111d238f808eefec1a447df1eb47
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-27 20:27:16 +0900

    ♠ UPDATE :: Validated pattern

--- src/main/kotlin/com/example/todolistv2/domain/auth/data/dto/request/CreateUserRequest.kt
+++ src/main/kotlin/com/example/todolistv2/domain/auth/data/dto/request/CreateUserRequest.kt
@@ -1,7 +1,16 @@
 package com.example.todolistv2.domain.auth.data.dto.request
 
+import javax.validation.constraints.Pattern
+
 data class CreateUserRequest(
+
     val nickname: String,
+
+    @field:Pattern(regexp = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$",
+        message = "이메일 형식이여야 합니다 (ex : abc123@gmail.com)")
     val accountId: String,
+
+    @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#\$%^&*()+|=]{8,30}\$",
+        message = "영 대/소문자, 숫자, 특수문자 포함 총 8자 이상 입력해야합니다")
     val password: String
 )
