aa92292fa5232104b3571b222dfdc1c5c5e94574
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-27 20:28:01 +0900

    ♠ UPDATE :: ErrorCode update

--- src/main/kotlin/com/example/todolistv2/global/error/data/ErrorCode.kt
+++ src/main/kotlin/com/example/todolistv2/global/error/data/ErrorCode.kt
@@ -6,6 +6,12 @@ enum class ErrorCode(
     val message: String,
     val status: HttpStatus
 ) {
+    //400
+    DUPLICATE_NICKNAME("duplicated nickname", HttpStatus.BAD_REQUEST),
+
     //401
-    UN_AUTHORIZED("", HttpStatus.UNAUTHORIZED)
+    UN_AUTHORIZED("un authorized", HttpStatus.UNAUTHORIZED),
+
+    //404
+    ENTITY_NOT_FOUND("entity not found", HttpStatus.NOT_FOUND)
 }
