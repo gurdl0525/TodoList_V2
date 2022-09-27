5c70b540ce61b553b72faf7e2141ccebc4d30d95
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-28 08:36:45 +0900

    ♠ UPDATE :: ConfigurationPropertiesScan Annotation 추가

--- src/main/kotlin/com/example/todolistv2/TodoListV2Application.kt
+++ src/main/kotlin/com/example/todolistv2/TodoListV2Application.kt
@@ -1,9 +1,11 @@
 package com.example.todolistv2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class TodoListV2Application
 
 fun main(args: Array<String>) {
