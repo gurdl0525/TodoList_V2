dd07ebcc139aaf9a8bb48fea61276e002481b7ae
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-28 08:39:14 +0900

    ♠ UPDATE :: 추상 메소드 추가

--- src/main/kotlin/com/example/todolistv2/domain/auth/repository/UserRepository.kt
+++ src/main/kotlin/com/example/todolistv2/domain/auth/repository/UserRepository.kt
@@ -3,7 +3,13 @@ package com.example.todolistv2.domain.auth.repository
 import com.example.todolistv2.domain.auth.data.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface UserRepository<T: User>: JpaRepository<T, Long> {
+
+    fun findByAccountId(accountId: String): Optional<T>
+
+    fun existsByNickname(nickname: String): Boolean
+
 }
