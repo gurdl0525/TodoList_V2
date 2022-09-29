d259f2b2612433a488382682be745dadfad9a742
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-29 23:02:39 +0900

    ♠ UPDATE :: NotBlank Annotation 추가

--- src/main/kotlin/com/example/todolistv2/domain/auth/data/dto/request/EditUserNickNameRequest.kt
+++ src/main/kotlin/com/example/todolistv2/domain/auth/data/dto/request/EditUserNickNameRequest.kt
@@ -1,5 +1,8 @@
 package com.example.todolistv2.domain.auth.data.dto.request
 
+import javax.validation.constraints.NotBlank
+
 data class EditUserNickNameRequest(
+    @field:NotBlank
     val nickname: String
 )
