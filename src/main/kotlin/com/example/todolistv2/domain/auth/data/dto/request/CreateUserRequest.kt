58d3928b8af2d3d61cbedbcfa1ff99a64dd635f4
squall7011 <gurdl7011@dsm.hs.kr>
2022-09-29 23:02:09 +0900

    ♠ UPDATE :: message 수정

--- src/main/kotlin/com/example/todolistv2/domain/auth/data/dto/request/CreateUserRequest.kt
+++ src/main/kotlin/com/example/todolistv2/domain/auth/data/dto/request/CreateUserRequest.kt
@@ -7,7 +7,7 @@ data class CreateUserRequest(
     val nickname: String,
 
     @field:Pattern(regexp = "^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$",
-        message = "이메일 형식이여야 합니다 (ex : abc123@gmail.com)")
+        message = "이메일 형식이여야 합니다 (ex :: abc123@gmail.com)")
     val accountId: String,
 
     @field:Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#\$%^&*()+|=]{8,30}\$",
